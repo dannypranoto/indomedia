723b935f8fbba106a0d4f4fd00a1425970bfacae
Danny Pranoto <danny.pranoto@veritrans.co.id>
2015-10-20 22:28:46 +0700

    Deleted pr newswire

--- wp-content/themes/colormag-child/sidebar.php
+++ wp-content/themes/colormag-child/sidebar.php
@@ -41,13 +41,5 @@
 
       endif; ?>
 
-      <h3 class="widget-title"><span>PR News Wire</span></h3>
-      <div class="pr-newswire-content">
-        <!-- PRNewswire Headlines Widget Code Starts Here -->
-        <script type="text/javascript" src="http://widget.prnewswire.com/widget/widget.js?&preset=2&expandheight=1&width=280&height=600&showlogos=1&categories=TEA,Entertainment,Food-and-Drink,Travel-and-Leisure,Technology-and_Gadgets,DATING,DOGS,GRANDPARENTS,HORSES,PREGNANCY-CHILDBIRTH-BABIES,TEENAGERS-HIGH-SCHOOL&industries=ADV,ART,BKS,ENT,FLM,MAG,MEN,MLM,MUS,PUB,RAD,SMD,TVN,AGR,ALC,BEV,FOD,NAB,ORF,RST,SUP,TOB,BIM,BIO,DEN,HEA,HIN,IDC,MTC,MEQ,MNH,PHA,SPM,SPT,SSE,SPE,BRD,HRD,NET,STW,CPR,CSE,ECM,ECP,EDA,GAM,EPM,HTS,ITE,NAN,PER,RFI,SWB,SEM,TLS,TCS,TEQ,VIP,WEB,WIC&regions=Indonesia&subjects=SVY,TRI&andorquestion=AND&numresults=5&numwords=10&hlineorbrief=both&bold=1&bkgcolor=FFFFFF&rowcolor1=FFFFFF&rowcolor2=FFFFFF&bordercolor=CCCCCC&link=289DCC&visitedcolor=FFFFFF&textcolor=444444&datecolor=888888&fontface=Trebuchet MS&fontsize=10&borderstyle=dotted&bordersize=1&p=prnewswire.html&"></script>
-        <!-- PRNewswire Headlines Widget Code Ends Here -->
-      </div>
-
-
 	<?php do_action( 'colormag_after_sidebar' ); ?>
 </div>
