1698858481675eba90882e8213569b13c049aac2
Danny Pranoto <danny.pranoto@veritrans.co.id>
2015-10-20 09:00:27 +0700

    Added some templates and sql dump

---
+++ wp-content/themes/colormag-child/page-templates/food.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: food Page Template
+ *
+ * Displays the food Page Template of the theme.
+ *
+ * @package ThemeGrill
+ * @subpackage ColorMag
+ * @since ColorMag 1.0
+ */
+?>
+
+<?php get_header(); ?>
+
+   <div class="main-content-section clearfix">
+      <div id="primary">
+         <div id="content" class="clearfix">
+
+         <?php
+         if( is_active_sidebar( 'colormag_food_page_content_top_section' ) ) {
+            if ( !dynamic_sidebar( 'colormag_food_page_content_top_section' ) ):
+            endif;
+         }
+         ?>
+         <div class="clearfix"></div>
+
+         <?php
+         if (get_theme_mod('colormag_hide_blog_front', 0) == 0): ?>
+
+         <?php endif; ?>
+         </div>
+      </div>
+      <?php colormag_sidebar_select(); ?>
+   </div>
+
+<?php get_footer(); ?>
